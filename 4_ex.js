8afe12df032fb25fdea71e5644b25743ab80be44
mia336love <122865270+mia336love@users.noreply.github.com>
2023-01-31 02:37:59 +0300

    4_ex.js (arithmetic average)

---
+++ 4_ex.js
@@ -0,0 +1,10 @@
+const a = -4;
+const b = 2;
+const c = 3;
+
+const num1 = Math.abs(a ** 3);
+const num2 = Math.abs(b ** 3);
+const num3 = Math.abs(c ** 3);
+
+const arith = (num1 + num2 + num3) / 3;
+console.log(arith);
