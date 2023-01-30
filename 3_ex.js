e047a32e89265475ffc2fcd2689cd8b6207366a0
mia336love <122865270+mia336love@users.noreply.github.com>
2023-01-31 00:31:08 +0300

    3_ex (spelling corrected)

--- 3_ex.js
+++ 3_ex.js
@@ -2,5 +2,5 @@ let n = 12.55;
 let m = 18.44;
 
 let result = (n + m) / 2;
-console.log("Дробная часть равноа: ", result % 1);
+console.log("Дробная часть равна: ", result % 1);
 //console.log("среднее арифметическое: ", (n + m) / 2);
