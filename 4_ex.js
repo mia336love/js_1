33ce4c2dce67df0e67bcd6be9dafb317c3ee2448
mia336love <122865270+mia336love@users.noreply.github.com>
2023-01-31 02:56:32 +0300

    4_ex.js (geometric mean)

--- 4_ex.js
+++ 4_ex.js
@@ -1,10 +1,17 @@
-const a = -4;
-const b = 2;
-const c = 3;
+let a = -4;
+let b = 2;
+let c = 3;
 
-const num1 = Math.abs(a ** 3);
-const num2 = Math.abs(b ** 3);
-const num3 = Math.abs(c ** 3);
+let num1 = Math.abs(a ** 3);
+let num2 = Math.abs(b ** 3);
+let num3 = Math.abs(c ** 3);
 
-const arith = (num1 + num2 + num3) / 3;
-console.log(arith);
+let arith = (num1 + num2 + num3) / 3;
+console.log("Среднее арифметическое модулей кубов данных чисел: ", arith);
+
+let num01 = a ** 2;
+let num02 = b ** 2;
+let num03 = c ** 2;
+
+let geom = Math.cbrt(num01 * num02 * num03);
+console.log("Среднее геомтрическое квадратов данных чисел равно: ", geom);
